8c4b555722e733f43996a2a032f749320003f7fc
Jakub Łobos <jakubloboskontakt@gmail.com>
2025-12-18 02:09:53 +0100

    fix server

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,6 @@ app.get("/nicki", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
